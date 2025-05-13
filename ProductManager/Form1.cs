4f3fe639b4d6414466b47b0171858002645d6f98
Logan Nelson <Lnelson0915@gmail.com>
2025-05-13 11:07:58 -0700

    Created a greeting message when the form is first loaded

--- ProductManager/Form1.cs
+++ ProductManager/Form1.cs
@@ -6,5 +6,10 @@ namespace ProductManager
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello Source Control!");
+        }
     }
 }
